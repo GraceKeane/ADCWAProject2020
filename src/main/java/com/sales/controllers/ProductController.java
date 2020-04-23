2d743e7ed4d59eb967996ca5e159362c818197ae
GraceKeane <gracekeane82@gmail.com>
2020-04-23 19:36:37 +0100

    Products can be successfully added to the web & database

--- src/main/java/com/sales/controllers/ProductController.java
+++ src/main/java/com/sales/controllers/ProductController.java
@@ -1,11 +1,15 @@
 package com.sales.controllers;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sales.models.Product;
 import com.sales.services.ProductService;
@@ -24,4 +28,36 @@ public class ProductController {
 		model.addAttribute("products", products);
 	    return "showProducts";
 	  }
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@RequestMapping(value = "/addProduct.html", method=RequestMethod.GET)
+	public String addProductGET(Model model) {
+		
+		ArrayList<Product> products = ps.getAllProducts();
+		
+		
+		
+		
+		Map<String, Integer> getProducts =
+				new LinkedHashMap<String, Integer>();
+		
+		for (Product p : products) {
+			getProducts.put(p.getProductname(), p.getStocknumber());
+		}
+		
+		model.addAttribute("getProducts", products);
+		
+		Product p = new Product();
+		model.addAttribute("product", p);
+		return "addProduct";
+	}
+	
+	@RequestMapping(value = "/addProduct.html", method=RequestMethod.POST)
+	public String addProductPOST(@ModelAttribute("product") Product p) {
+		ps.saveProduct(p);
+		return "redirect:getProducts.html";
+		
+	}
+	
 }
