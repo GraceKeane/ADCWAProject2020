e31dd7255c1c49757b4997857e6a2e2f4808627e
GraceKeane <gracekeane82@gmail.com>
2020-05-01 10:14:30 +0100

    Add order & date full working

--- src/main/java/com/sales/services/OrderService.java
+++ src/main/java/com/sales/services/OrderService.java
@@ -1,6 +1,9 @@
 package com.sales.services;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,12 +20,17 @@ public class OrderService {
 	@Autowired
 	OrderRepo or;
 	
+	// get current date 
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	private Date date = new Date();
+		
 	public ArrayList<Order> getAllOrders() {
 		return (ArrayList<Order>) or.findAll();
 		
 	}
 	
 	public void save(Order o) {
+		o.setOrderDate(dateFormat.format(date));
 		or.save(o);
 		
 		System.out.printf("Order saved");
